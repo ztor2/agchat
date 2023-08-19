b2a78b6ee15749ef408f0898e5aa86bf207b4b26
unknown <ztor2>
2023-08-19 13:22:13 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head/>
-      <title>AgensSQL QA Chatbot</title>
       <body>
         <Main />
         <NextScript />
