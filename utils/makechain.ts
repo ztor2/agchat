5394662f9934a53e1bc286354606339817061098
unknown <ztor2>
2023-08-19 12:24:39 +0900

    update

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -24,7 +24,7 @@ let streamedResponse = "";
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new ChatOpenAI({
     temperature: 0.25, // increase temepreature to get more creative answers
-    modelName: 'gpt-4', //change this to gpt-4 if you have access
+    modelName: 'gpt-3.5-turbo-16k', //change this to gpt-4 if you have access
     streaming: true,
     callbacks: [{
       handleLLMNewToken(token) {
