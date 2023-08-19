a9701955c7955c4f3f96034617b875e8b2a91feb
unknown <ztor2>
2023-08-19 13:17:02 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>AgensSQL QA Chatbot</title>
       <Head />
       <body>
         <Main />
--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,7 +1,6 @@
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
-import { CallbackManager } from "langchain/callbacks";
 
 export const runtime = 'edge'
 
