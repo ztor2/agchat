3fdbfa5a1ca44d717dc4ff092e48d0830175926c
unknown <ztor2>
2023-08-19 13:18:47 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <Head/>
       <title>AgensSQL QA Chatbot</title>
-      <Head />
       <body>
         <Main />
         <NextScript />
